08a1180d5d2291be2e0c5d2abeb7df2e0bc57ceb
Michael Havey - Fitzgerald <michaelhavey-fitzgerald@Michaels-MBP.wework.com>
2019-11-07 17:41:55 +0000

    feat: new design

--- src/containers/landing/Page.vue
+++ src/containers/landing/Page.vue
@@ -1,5 +1,24 @@
 <template>
 	<div class="LandingPage">
-		<img alt="Vue logo" src="../../assets/logo.png" />
+		<h1 class="LandingPage__title">Holistic Web</h1>
 	</div>
 </template>
+
+<style lang="scss">
+.LandingPage {
+	min-width: 100%;
+	min-height: 100vh;
+	background-image: url('../../assets/road.jpg');
+	background-size: cover;
+	background-position: center;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+
+	&__title {
+		font-size: 5rem;
+		color: #fff;
+	}
+}
+</style>
