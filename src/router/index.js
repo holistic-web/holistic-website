210008fc71c69b6001c474392c58b4b3600fd174
Michael Havey - Fitzgerald <michaelhavey-fitzgerald@Michaels-MBP.wework.com>
2019-11-07 17:11:33 +0000

    chore: add basic content

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
-import About from '../views/About.vue';
+import LandingPage from '../containers/landing/Page.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
 	{
 		path: '/',
-		name: 'home',
-		component: Home
-	},
-	{
-		path: '/about',
-		name: 'about',
-		component: About
+		name: 'landing.page',
+		component: LandingPage
 	}
 ];
 
