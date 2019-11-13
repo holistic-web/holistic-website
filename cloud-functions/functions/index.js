ab0f6bd6ea35706a7ee4efdddbd326627cf0e867
Dan Bennett <dghbennett47@gmail.com>
2019-11-13 17:36:49 +0000

    feat: add cloud function for sending emails

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -1,8 +1,33 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+
+exports.sendEmail = functions.https.onCall(async (emailData, context) => {
+	console.log('> sendEmail called with: ' + JSON.stringify(emailData, null, 4));
+	const { message, name, senderEmail, receiptEmail } = emailData;
+	const email = functions.config().email;
+
+
+	const mailTransport = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+            user: email.address,
+            pass: email.password
+		}
+	});
+
+	const mailOptions = {
+		from: `${name} <${senderEmail}>`,
+		to: receiptEmail,
+		subject: `${name} filled out a contact form at Holistic Web`,
+		text: message
+	};
+
+	await mailTransport.sendMail(mailOptions);
+	return 'success'
+});
