a0403852fc0352342b6b0f4f699c3a84fe0ee711
Dan Bennett <dghbennett47@gmail.com>
2019-11-22 17:55:03 +0000

    chore: removed prescription code

--- src/containers/landing/Page.vue
+++ src/containers/landing/Page.vue
@@ -7,19 +7,6 @@
 			we aim to satisfy your needs with a quality affordable bespoke software solution.
 		</div>
 
-
-<!-- 		<div class="PrescriptionPortfolio">
-
-		<section class="PrescriptionPortfolio__content">
-			<section class="PrescriptionPortfolio__content__text">
-				This innovative service was developed to give doctors the ability to care more directly for patients.
-				A doctor can create an individual schedule for each patient, issuing prescriptions,
-				and questions about the patient's condition and scheduling appointments.
-			</section>
-		</section>
-		<section class="PrescriptionPortfolio__content__prescription-image"/>
-		</div> -->
-
 	</div>
 </template>
 
@@ -50,39 +37,5 @@ export default {
 	}
 }
 
-.PrescriptionPortfolio {
-	min-width: 100%;
-	min-height: 50vh;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
 
-
-	&__content {
-		height: 50vh;
-		padding: $theme-page-padding;
-
-		&__text {
-			padding: 10rem 4rem;
-			position: absolute;
-			left: 0px;
-			width: 45%;
-			text-align: center;
-			color: $colour-blue-light;
-			font-size: $theme-font-size-large;
-		}
-		&__prescription-image {
-		position: absolute;
-		bottom: -550px;
-		right: 10px;
-		background-repeat: no-repeat;
-		background-size: contain;
-		background-image: url('../../assets/prescription-image.png');
-		width: 950px;
-		height: 50vh;
-	}
-	}
-
-}
 </style>
