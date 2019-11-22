afa2102dad0b1bba4a0a27e565b0dc1d51dba6ab
Dan Bennett <dghbennett47@gmail.com>
2019-11-22 16:56:11 +0000

    feat: add prescription example

--- src/containers/landing/Page.vue
+++ src/containers/landing/Page.vue
@@ -2,10 +2,18 @@
 	<div class="LandingPage">
 
 		<hero />
+		<div class="PrescriptionPortfolio">
 
-		<section class="LandingPage__content">
-			lorem bacon and ting - wagwan.
+		<section class="PrescriptionPortfolio__content">
+			<section class="PrescriptionPortfolio__content__text">
+				This innovative service was developed to give doctors the ability to care more directly for patients.
+				A doctor can create an individual schedule for each patient, issuing prescriptions,
+				and questions about the patient's condition and scheduling appointments.
+			</section>
 		</section>
+		<section class="PrescriptionPortfolio__content__prescription-image"/>
+		</div>
+
 	</div>
 </template>
 
@@ -21,16 +29,47 @@ export default {
 
 <style lang="scss" scoped>
 @import '../../styles/theme';
+@import '../../styles/colours';
 
 .LandingPage {
 	display: flex;
 	justify-content: center;
 	flex-direction: column;
+}
+
+.PrescriptionPortfolio {
+	min-width: 100%;
+	min-height: 50vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
 
 	&__content {
-		width: $theme-page-width-desktop;
+		height: 50vh;
 		padding: $theme-page-padding;
-		text-align: left;
+
+		&__text {
+			padding: $theme-prescription-padding;
+			position: absolute;
+			left: 0px;
+			width: 45%;
+			text-align: center;
+			color: $colour-blue-light;
+			font-size: $theme-font-size-large;
+		}
+		&__prescription-image {
+		position: absolute;
+		bottom: -550px;
+		right: 10px;
+		background-repeat: no-repeat;
+		background-size: contain;
+		background-image: url('../../assets/prescription-image.png');
+		width: 950px;
+		height: 50vh;
 	}
+	}
+
 }
 </style>
