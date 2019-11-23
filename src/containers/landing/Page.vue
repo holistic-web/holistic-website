40b2667b543c9fd2a2928d683d905b6435ff68e0
Dan Bennett <dghbennett47@gmail.com>
2019-11-23 19:11:43 +0000

    feat: add our work section

--- src/containers/landing/Page.vue
+++ src/containers/landing/Page.vue
@@ -9,6 +9,11 @@
 				We are Holistic Web, a software development and web design collective with offices in London and Los Angeles. As an international team of experienced, enthusiastic developers,
 				we aim to satisfy your needs with a quality affordable bespoke software solution.
 			</p>
+			<h3 class="LandingPage__aboutUs__title">Our work</h3>
+			<p class="LandingPage__aboutUs">
+				We love web development and understand the value of a modern, innovative and polished website in this digital age.
+				We cater to businesses or individuals and specialize at producing a professional brand-enhancing product for a variety of budgets. Take a look at some of our projects:
+			</p>
 		</section>
 
 	</div>
@@ -43,18 +48,21 @@ export default {
 		color: $colour-white;
 		font-size: $theme-font-size-large;
 		text-align: left;
+		margin-top: 0.5rem;
+		float: right;
 
 		&__title {
 			font-family: $theme-font-title;
 			text-align: left;
 			margin: 0;
-			margin-bottom: 2rem;
+			margin-bottom: 1.5rem;
 			font-size: $theme-font-size-large;
 			text-transform: uppercase;
 			letter-spacing: 0.1rem;
 			color: $colour-blue-light;
 		}
 	}
+
 }
 
 
